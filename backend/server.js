8c23da5e4df117109a4f3c28eefd17452b907ff7
Anselmo <anselmoma2005@outlook.com>
2024-11-19 13:53:52 -0300

    update repo

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+const posts = [
+  {
+    id: 1,
+    descricao: 'um teste do post',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    id: 2,
+    descricao: 'Descrição do segundo post',
+    imagem: 'https://placecats.com/200/300'
+  },
+  {
+    id: 3,
+    descricao: 'Terceiro post: Uma imagem legal',
+    imagem: 'https://placekitten.com/400/200'
+  },
+  {
+    id: 4,
+    descricao: 'Post 4: Mais um exemplo',
+    imagem: 'https://picsum.photos/id/237/200/300'
+  },
+  {
+    id: 5,
+    descricao: 'Último post da lista',
+    imagem: 'https://source.unsplash.com/random/200x300'
+  }
+];
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server listening...');
+});
+
+app.get('/posts/:id', (req, res) => {
+  res.status(200).json(posts);
+});
---
+++ db/db_alura.sql
@@ -0,0 +1,5 @@
+drop database dbalura;
+create database dbalura;
+show databases;
+
+use dbalura;
