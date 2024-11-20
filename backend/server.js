aea570f38b0c7e09cd86598790702142460a9153
Anselmo <anselmoma2005@outlook.com>
2024-11-20 13:43:46 -0300

    add postsdb connection func at server.js

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,6 @@
 import express from 'express';
-
-const posts = [
-  {
-    id: 1,
-    descricao: 'um teste do post',
-    imagem: 'https://placecats.com/millie/300/150'
-  },
-  {
-    id: 2,
-    descricao: 'Descrição do segundo post',
-    imagem: 'https://placecats.com/200/300'
-  },
-  {
-    id: 3,
-    descricao: 'Terceiro post: Uma imagem legal',
-    imagem: 'https://placekitten.com/400/200'
-  },
-  {
-    id: 4,
-    descricao: 'Post 4: Mais um exemplo',
-    imagem: 'https://picsum.photos/id/237/200/300'
-  },
-  {
-    id: 5,
-    descricao: 'Último post da lista',
-    imagem: 'https://source.unsplash.com/random/200x300'
-  }
-];
+import conectarAoBanco from './src/config/dbConfig.js';
+const conexao = await conectarAoBanco(process.env.CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
@@ -35,17 +9,24 @@ app.listen(3000, () => {
   console.log('Server listening...');
 });
 
-app.get('/posts/', (req, res) => {
+async function getAllPosts() {
+  const db = conexao.db('imersion-instabytes');
+  const colecao = db.collection('posts');
+  return colecao.find().toArray();
+};
+
+app.get('/posts', async (req, res) => {
+  const posts = await getAllPosts();
   res.status(200).json(posts);
 });
 
-function SearchPostByID(id){
-  return posts.findIndex((posts)=> {
-    return posts.id === Number(id);
-  });
-};
+// function SearchPostByID(id){
+//   return posts.findIndex((posts)=> {
+//     return posts.id === Number(id);
+//   });
+// };
 
-app.get('/posts/:id', (req, res) => {
-  const index = SearchPostByID(req.params.id);
-  res.status(200).json(posts[index]);
-});
+// app.get('/posts/:id', (req, res) => {
+//   const index = SearchPostByID(req.params.id);
+//   res.status(200).json(posts[index]);
+// });
