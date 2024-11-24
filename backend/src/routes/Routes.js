b34a99b415cad382e805a42ce930768c5e2df251
Anselmo <anselmoma2005@outlook.com>
2024-11-24 10:55:25 -0300

    add route DELETE at Routes.js

--- backend/src/routes/Routes.js
+++ backend/src/routes/Routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import cors from 'cors';
-import { ListPosts, CriarNovoPost, UploadImagem, AtualizaNovoPost } from '../controller/Controller.js';
+import { ListPosts, CriarNovoPost, UploadImagem, AtualizaNovoPost, DeletarPost } from '../controller/Controller.js';
 
 const corsOptions = {
   origin : "http://localhost:8000",
@@ -17,6 +17,7 @@ const Routes = (app) => {
   app.post('/posts', CriarNovoPost); // Define uma rota POST para enviar um novo objeto para o endpint '/posts'
   app.post('/upload', upload.single('imagem'), UploadImagem); // Define uma rota POST para enviar um novo objeto através da função UploadImagem
   app.put('/upload/:id', AtualizaNovoPost); // define uma rota do tipo PUT para um objeto referenciado pelo id na url
+  app.delete('/delete/:id', DeletarPost);
 }
 
 export default Routes;
