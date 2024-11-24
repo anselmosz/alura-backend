d9bf979004fd16c2deee6260a710b19c391e023f
Anselmo <anselmoma2005@outlook.com>
2024-11-24 09:45:48 -0300

    update postModels.js by adding comments

--- backend/src/models/postsModel.js
+++ backend/src/models/postsModel.js
@@ -11,15 +11,27 @@ export async function getAllPosts() {
 };
 
 export async function CriarPost(novoPost) {
+  // Conecta ao banco de dados 'imersion-instabytes'
   const db = conexao.db('imersion-instabytes');
+
+  // Seleciona a coleção 'posts' dentro do banco de dados
   const colecao = db.collection('posts');
 
+  // Insere o novo post na coleção 'posts' e retorna o resultado da operação
   return colecao.insertOne(novoPost);
-};
+}
 
 export async function AtualizarPost(id, novoPost) {
+  // Conecta ao banco de dados 'imersion-instabytes'
   const db = conexao.db('imersion-instabytes');
+
+  // Seleciona a coleção 'posts' dentro do banco de dados
   const colecao = db.collection('posts');
+
+  // Converte o ID do post em um objeto ObjectId do MongoDB
   const objID = ObjectId.createFromHexString(id);
+
+  // Atualiza o post com o ID especificado, substituindo os campos com os novos valores
   return colecao.updateOne({_id: new ObjectId(objID)}, {$set: novoPost});
-};
+}
+
